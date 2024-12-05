e26e3afd789cf01577b9850b348d6b15bdc2496c
Jason Hale <jason.hale@evanshunt.com>
2024-12-05 10:55:33 -0700

    Added dark / light mode colors

--- src/components/letter.tsx
+++ src/components/letter.tsx
@@ -8,12 +8,14 @@ export type LetterProps = {
 
 export const Letter: Devvit.BlockComponent<LetterProps> = ({letter}) => {
 
+    const theme = "dark";
+
     if(letter == " ")
         return <></>;
 
     return (
-        <hstack width="30px" height="40px" alignment="center middle" gap="none" border="thick" cornerRadius="small" borderColor="global-black">
-            <text size="xxlarge" weight="bold" alignment="center middle">{letter}</text>
+        <hstack width="30px" height="40px" alignment="center middle" gap="none" border="thick" cornerRadius="small" borderColor="global-black" darkBackgroundColor='global-white' lightBackgroundColor='global-black'>
+            <text size="xxlarge" weight="bold" alignment="center middle" lightColor='global-white' darkColor='global-black'>{letter}</text>
         </hstack>
     );
 };
